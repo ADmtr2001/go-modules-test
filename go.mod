3408a6349a549884d5586544e0ee7ffdc2717140
Arsen Dmytrenko <arsen.dmitrenko1994@gmail.com>
2023-09-02 22:23:28 +0300

    drop by drop, the bucket gets filled

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module mod-and-dep-management
 
 go 1.20
+
+require github.com/ADmtr2001/puppy v0.0.0-20230902190812-ce9f4b306d94
+
+require github.com/ADmtr2001/dog v0.0.0-20230902190143-a804a29c9254 // indirect
--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/ADmtr2001/puppy"
+)
+
+func main() {
+	s1 := puppy.Bark()
+	s2 := puppy.Barks()
+	s3 := puppy.BigBark()
+	s4 := puppy.BigBarks()
+
+	fmt.Println(s1)
+	fmt.Println(s2)
+	fmt.Println(s3)
+	fmt.Println(s4)
 }
